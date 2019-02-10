beed56bcb3d49810b64f8ca2a8f53cc6f1bf1d4a
Jefferson <jeffersonhcarvalho@gmail.com>
2019-02-10 16:22:45 -0300

    Updates

---
+++ precache-manifest.81c49532d54d6f5d133d0c8477cdf4e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5ce03d66e793e6ad514",
+    "url": "/astrocoders-test/static/js/runtime~main.e5ce03d6.js"
+  },
+  {
+    "revision": "5634d5babe1449f828a6",
+    "url": "/astrocoders-test/static/js/main.5634d5ba.chunk.js"
+  },
+  {
+    "revision": "51ca5a63c5fdaa13d884",
+    "url": "/astrocoders-test/static/js/1.51ca5a63.chunk.js"
+  },
+  {
+    "revision": "dd68ca85b2c5337864c4b3b688b797c7",
+    "url": "/astrocoders-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/astrocoders-test/precache-manifest.5b4e859597c37c2fbe695bd708677fdf.js"
+  "/astrocoders-test/precache-manifest.81c49532d54d6f5d133d0c8477cdf4e7.js"
 );
 
 workbox.clientsClaim();
